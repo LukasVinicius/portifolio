d3a67d15c27e1d00e6c5d9a048dd6747bbf4c518
Lukas Vinícius <lukviniaran@gmail.com>
2025-07-29 20:21:18 -0300

    .\gitFlow.ps1

--- gitflow.ps1
+++ gitflow.ps1
@@ -4,3 +4,6 @@ $mensagem = Read-Host "Digite a mensagem do commit"
 git commit -m "$mensagem"
 git pull origin main
 git push origin main
+
+# git status && git add . && git commit -m "Mensagem do commit" && git pull origin main && git push origin main (Git Bash)
+# git status; git add .; git commit -m "Mensagem do commit"; git pull origin main; git push origin main (Power Shell)
